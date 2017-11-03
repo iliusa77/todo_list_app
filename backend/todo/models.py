4d085a8d61224e326dce1729076607502a4a722e
Your Name <you@example.com>
2017-11-03 16:56:33 +0200

    initial commit

---
+++ backend/todo/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class GroupTask(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=256)
+    group = models.ForeignKey(GroupTask, on_delete=models.CASCADE, related_name='tasks')
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=32)
+    task = models.ManyToManyField(Task)
+
+    def __str__(self):
+        return self.name
+
