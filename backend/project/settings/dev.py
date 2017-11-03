88e189ae33d0258987e08cc793d521e9febe16f0
Your Name <you@example.com>
2017-11-03 22:18:34 +0200

    swithed to mysql, added mysqlclient

--- backend/project/settings/dev.py
+++ backend/project/settings/dev.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+     'default': dj_database_url.config()
 }
 
 
