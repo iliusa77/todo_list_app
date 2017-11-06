9aca6d62ca6aa90d43779f361c702349b3edfd7a
Your Name <you@example.com>
2017-11-06 14:17:48 +0200

    separated function  getTasksList and getGroupsList

--- frontend/app.js
+++ frontend/app.js
@@ -2,18 +2,27 @@ var app = new Vue({
         el: '#tasks',
 
         data: {
-            tasks: []
+            tasks: [],
+            groups: []
         },
 
         created: function () {
-            this.fetchData();
+            this.getTasksList();
+            this.getGroupsList();
         },
 
         methods: {
-            fetchData: function () {
+            getTasksList: function () {
                 this.$http.get('http://127.0.0.1:8000/tasks/')
                           .then(response => {
                              this.tasks = response.data
+                             // or like this this.getTemp = response.json()
+                          })
+            },
+            getGroupsList: function () {
+                this.$http.get('http://127.0.0.1:8000/grouptasks/')
+                          .then(response => {
+                             this.groups = response.data
                              // or like this this.getTemp = response.json()
                           })
             }
